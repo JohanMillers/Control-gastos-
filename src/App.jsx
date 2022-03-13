b08d3d94e096cc251edcfe8a9b674370793da014
Johan <johanoscarmarte@gmail.com>
2022-03-13 00:49:23 -0400

    Colocando presupuerto en el LS

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ function App() {
 
   const [gastos, setGastos] = useState([]);
 
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto'))?? 0
+  );
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false)
@@ -22,13 +24,27 @@ function App() {
   useEffect(() => {
     if (Object.keys(gastoEditar).length > 0) {
       setModal(true)
-  
       setTimeout(() => {
         setAnimarModal(true);
       }, 500);
     }
     
-  },[gastoEditar])
+  }, [gastoEditar])
+  
+  useEffect(() => {
+    localStorage.setItem('presupuesto', presupuesto ?? 0)
+    
+  }, [presupuesto])
+  
+  useEffect(() => {
+
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+
+    if (presupuestoLS > 0) {
+      setIsValidPresupuesto(true)
+    }
+    
+  },[])
 
 
   
--- src/componetes/ControlPresupuesto.jsx
+++ src/componetes/ControlPresupuesto.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { CircularProgressbar } from 'react-circular-progressbar'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import "react-circular-progressbar/dist/styles.css"
 
 const ControlPresupuesto = ({ gastos, presupuesto }) => {
@@ -35,7 +35,15 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
       <div className="contenedor-presupuesto contenedor sombra dos-columnas">
           <div>
               <CircularProgressbar
+                  styles={buildStyles({
+                      pathColor: '#3B82f6',
+                      trailColor: '#f5f5f5f5',
+                      textColor: '#3B82f6'
+
+
+                  })}
                   value={porcentaje}
+                  text={`${porcentaje}% Gastado`}
               
               />
           </div>
